cd6a1e17fe479e8fff8eae8433efd8a3bfda0494
Haseeb Majid <hello@haseebmajid.dev>
2024-08-21 21:48:37 +0100

    wor

--- internal/transport/websockets/subscribe_integration_test.go
+++ internal/transport/websockets/subscribe_integration_test.go
@@ -65,4 +65,57 @@ func TestIntegrationSubscribe(t *testing.T) {
 		assert.Contains(t, msg, playerNickname)
 		assert.Regexp(t, "Code: [A-Z0-9]{5}", msg)
 	})
+
+	// t.Run("Should successfully handle join room message", func(t *testing.T) {
+	// 	playerNickname := "test1"
+	// 	message := map[string]string{
+	// 		"game_name":       "fibbing_it",
+	// 		"player_nickname": playerNickname,
+	// 		"message_type":    "create_room",
+	// 	}
+	//
+	// 	jsonMessage, err := json.Marshal(message)
+	// 	require.NoError(t, err)
+	//
+	// 	err = wsutil.WriteClientText(conn, jsonMessage)
+	// 	require.NoError(t, err)
+	//
+	// 	m, _, err := wsutil.ReadServerData(conn)
+	// 	require.NoError(t, err)
+	// 	msg := string(m)
+	//
+	// 	pattern := `Code: [A-Z0-9]{5}`
+	//
+	// 	re, err := regexp.Compile(pattern)
+	// 	if err != nil {
+	// 		fmt.Println("Error compiling regex:", err)
+	// 		return
+	// 	}
+	//
+	// 	matches := re.FindAllString(msg, -1)
+	// 	var roomCode string
+	// 	for _, match := range matches {
+	// 		roomCode = match
+	// 	}
+	//
+	// 	playerNickname = "test"
+	// 	message = map[string]string{
+	// 		"room_code":       roomCode,
+	// 		"player_nickname": playerNickname,
+	// 		"message_type":    "join_room",
+	// 	}
+	// 	//
+	// 	jsonMessage, err = json.Marshal(message)
+	// 	require.NoError(t, err)
+	//
+	// 	err = wsutil.WriteClientText(conn, jsonMessage)
+	// 	require.NoError(t, err)
+	//
+	// 	m, _, err = wsutil.ReadServerData(conn)
+	// 	require.NoError(t, err)
+	// 	msg = string(m)
+	//
+	// 	assert.Contains(t, msg, playerNickname)
+	// 	assert.Regexp(t, "Code: [A-Z0-9]{5}", msg)
+	// })
 }
